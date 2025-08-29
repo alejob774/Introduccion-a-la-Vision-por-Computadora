7b3140014ab2f467b1023ce36ae5c3e031be3d61
alejob774 <115667579+alejob774@users.noreply.github.com>
2025-08-29 07:43:28 -0500

    Update __init__.py

--- cvtools/__init__.py
+++ cvtools/__init__.py
@@ -1,1 +1,10 @@
+"""
+cvtools - Librería de visión por computador
+Módulos para procesamiento de imágenes y visión computacional
+"""
 
+from .camera import *
+from .color import *
+from .filters import *
+
+__version__ = "1.0.0"
